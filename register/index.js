cee268674bdb9fb46204f32ea6c0166ac7b26d73
LichyWons <124511088+LichyWons@users.noreply.github.com>
2026-01-14 23:07:31 +0100

    email and password validation

--- register/index.js
+++ register/index.js
@@ -1,11 +1,27 @@
 const form = document.getElementById('register-form');
-const errorMessage = document.getElementById('form-error');
+const errorMessage = document.getElementById('form-error-mail');
+const passwordErrorMessage = document.getElementById('form-error-password');
+
 form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  errorMessage.textContent = '';
+  passwordErrorMessage.textContent = '';
   const username = form.elements['username'].value;
   const email = form.elements['email'].value;
   const password = form.elements['password'].value;
-  event.preventDefault();
-  errorMessage.textContent = ' ';
-  console.log('register klikniety');
-  console.log(username, email, password);
+  const confirmPassword = form.elements['confirm-password'].value;
+
+  if (!email.endsWith('@stud.noroff.no')) {
+    errorMessage.textContent =
+      ' Achtung!!! Email must be a stud.noroff.no address.';
+    console.log('wrong email');
+    return;
+  }
+  console.log('correct email');
+  if (password !== confirmPassword) {
+    passwordErrorMessage.textContent = 'Passwords do not match.';
+    console.log('passwords do not match');
+    return;
+  }
+  console.log('passwords match');
 });
